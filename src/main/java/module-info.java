a4aa94681b806f34510cc4d087e50b463d880af2
VertUnix <41397224+VertUnix@users.noreply.github.com>
2021-01-15 17:47:40 +0200

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module ro.mta.se.lab {
-    requires javafx.controls;
-    requires javafx.fxml;
-    opens ro.mta.se.lab.controller;
-    opens ro.mta.se.lab to javafx.fxml;
-    exports ro.mta.se.lab;
-}
